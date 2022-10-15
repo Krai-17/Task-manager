bdfc78a8837a854ac3da2147dbef450d68e50803
Krai-17 <86406637+Krai-17@users.noreply.github.com>
2022-10-15 13:30:26 +0530

    This is my project first

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+const state = {
+    taskList: [],
+};
+
+// DOM = document object
+const taskContents = document.querySelector(".task_contents");
+const taskModal = document.querySelector(".task_modal_body");
